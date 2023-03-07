02a09e4baa3c9e9499efe07d4cf714feca94737e
Дмитрий <region29@bk.ru>
2023-03-07 03:10:10 +0300

    проверить дз

--- src/Author.java
+++ src/Author.java
@@ -1,8 +1,10 @@
 public class Author {
     private String lastName;
-    public Author (String lastName) {
+
+    public Author(String lastName) {
         this.lastName = lastName;
     }
+
     public String getLastName() {
         return lastName;
 
--- src/Book.java
+++ src/Book.java
@@ -2,19 +2,23 @@ import javax.lang.model.element.Name;
 
 public class Book {
     private String title;
-    private String  author;
+    private String author;
     private int year;
+
     public Book(String author, String title, int year) {
         this.author = author;
         this.title = title;
         this.year = year;
     }
-    public String  getAuthor() {
+
+    public String getAuthor() {
         return author;
     }
+
     public String getTitle() {
         return title;
     }
+
     public int getYear() {
         return year;
     }
